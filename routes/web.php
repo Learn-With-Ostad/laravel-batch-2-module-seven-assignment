7e35d06b399a9c260f91afcaab9326fcb6a1bfb1
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-11-12 13:09:54 +0600

    response in root route

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/hello', [UserController::class,'index']);
+Route::get('/', [UserController::class,'index']);
